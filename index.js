dd89ee457ec35983d25f7159f34ef15414b6c8d7
H_A _Opu <wwwhasib4@gmail.com>
2022-04-23 08:41:22 +0600

    some changes at app.listen

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("gallo world");
 });
-app.listen(port, (req, res) => {
+app.listen(port, () => {
   console.log("Listening Port:", port);
 });
